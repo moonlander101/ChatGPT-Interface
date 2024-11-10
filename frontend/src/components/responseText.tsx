4d224c4b0fa74752b95ee801586f881e5aa9767e
moonlander101 <114925949+moonlander101@users.noreply.github.com>
2024-11-10 22:32:00 +0530

    Changes

--- frontend/src/components/responseText.tsx
+++ frontend/src/components/responseText.tsx
@@ -3,7 +3,7 @@ import ReactMarkdown from 'react-markdown'
 
 function ResponseText({ message } : {message : string}) {
     return ( 
-        <div className="flex justify-center pt-2">
+        <div className="flex justify-center">
             <div className="w-4/5 max-w-[720px] flex p-2">
                 <div className="rounded-3xl w-[100%] h-auto text-white pl-6 pr-6 pt-3 pb-3">
                     <ReactMarkdown>
