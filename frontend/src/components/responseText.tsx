5c9c3fc3ed15ed4c18e824b6d8884c64601a68e0
moonlander101 <114925949+moonlander101@users.noreply.github.com>
2024-11-11 02:06:14 +0530

    Fixed markdown styles a bit

--- frontend/src/components/responseText.tsx
+++ frontend/src/components/responseText.tsx
@@ -6,9 +6,11 @@ function ResponseText({ message } : {message : string}) {
         <div className="flex justify-center">
             <div className="w-4/5 max-w-[720px] flex p-2">
                 <div className="rounded-3xl w-[100%] h-auto text-white pl-6 pr-6 pt-3 pb-3">
-                    <ReactMarkdown>
-                        {message}
-                    </ReactMarkdown>
+                    <div className='markdown'>
+                        <ReactMarkdown>
+                            {message}
+                        </ReactMarkdown>
+                    </div>
                 </div>
             </div>
         </div>
