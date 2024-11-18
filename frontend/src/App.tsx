f0e356d5ad8fc654270bfd8e074f412c134a0c3a
moonlander101 <114925949+moonlander101@users.noreply.github.com>
2024-11-18 15:42:36 +0530

    Added support to render math

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,6 @@ import Navbar from './components/Navbar';
 import TextInput from './components/TextInput';
 import Sidebar from './components/Sidebar';
 import ScrollButton from './components/ScrollButton';
-import { markdownString } from './components/Test';
 
 interface Message {
   role: string;
@@ -18,6 +17,7 @@ function App() {
   const scrollRef = useRef<HTMLDivElement>(null);
 
   const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const [isGenerating, setIsGenerating] = useState<boolean>(false);
   const [oldMessages, setOldMessages] = useState<Message[]>([]);
   const [isAtBottom, setIsAtBottom] = useState<boolean>(true);
   const [sidebarOpen, setSidebarOpen] = useState<boolean>(false);
@@ -67,11 +67,15 @@ function App() {
     const reader = aiRes.body!.getReader();
     const decoder = new TextDecoder();
 
+    setIsGenerating(true);
     while (true) {
         const { value, done } = await reader.read();
-        if (done) break;
+        if (done) {
+          setIsGenerating(false);
+          break;
+        };
 
-        const chunk = decoder.decode(value, { stream: true });
+        const chunk = decoder.decode(value, { stream: true })  
 
         setOldMessages(prevMessages => {
           const updatedMessages = [...prevMessages];
@@ -86,9 +90,23 @@ function App() {
         //   scrollToEnd();
         // }
     }
-    
+
+    // Delimiters for math
+    setOldMessages(prevMessages => {
+      const updatedMessages = [...prevMessages];
+      updatedMessages[updatedMessages.length - 1] = {
+        ...updatedMessages[updatedMessages.length - 1],
+        content: updatedMessages[updatedMessages.length - 1].content
+        .replace(/\\\(/g, '$$')
+        .replace(/\\\)/g, '$$')
+        .replace(/\\\[/g, '$$$$')
+        .replace(/\\\]/g, '$$$$')
+      ,
+      };
+      return updatedMessages;
+    });
+
     setIsSubmitting(false);
-    
     // scrollToEnd();
   }
 
@@ -127,32 +145,34 @@ function App() {
     //     cur.removeEventListener('scroll', handleScroll);
     //   }
     // };
-
+    if (oldMessages.length !== 0) {
+        console.log(oldMessages);
+    }
   }, [oldMessages]);
 
   return (
     <>
-      <div className='w-[100%] h-screen flex justify-evenly'>
-        <Sidebar sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar}/>
-        <div className={`relative w-[100%]`}>
-          <Navbar toggleSidebar={toggleSidebar} sidebarOpen={sidebarOpen} handleClear={handleClear}/>
-          <div className="bg-[#212121] flex h-screen flex-col justify-between">
-            <div className="flex-1 w-auto overflow-y-auto" ref={scrollRef}>
-              <div className='w-full h-10 bg-inherit'></div>
-              {oldMessages.map((message, index) => (
-                message.role === "user"
-                  ? <UserTextBubble key={index} message={message.content} />
-                  : <ResponseText key={index} message={message.content} />
-              ))}
-              <div ref={lastMessageRef}/>
-            </div>
-            <div className="w-full pb-7 pt-4 relative">
-              <ScrollButton isAtBottom={isAtBottom} onClick={scrollToEnd}/>
-              <TextInput onSubmit={handleSubmit} isSubmitting={isSubmitting}/>
+        <div className='w-[100%] h-screen flex justify-evenly'>
+          <Sidebar sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar}/>
+          <div className={`relative w-[100%]`}>
+            <Navbar toggleSidebar={toggleSidebar} sidebarOpen={sidebarOpen} handleClear={handleClear}/>
+            <div className="bg-[#212121] flex h-screen flex-col justify-between">
+              <div className="flex-1 w-auto overflow-y-auto" ref={scrollRef}>
+                <div className='w-full h-10 bg-inherit'></div>
+                {oldMessages.map((message, index) => (
+                  message.role === "user"
+                    ? <UserTextBubble key={index} message={message.content} />
+                    : <ResponseText key={index} message={message.content} isGenerating={isGenerating}/>
+                ))}
+                <div ref={lastMessageRef}/>
+              </div>
+              <div className="w-full pb-7 pt-4 relative">
+                <ScrollButton isAtBottom={isAtBottom} onClick={scrollToEnd}/>
+                <TextInput onSubmit={handleSubmit} isSubmitting={isSubmitting}/>
+              </div>
             </div>
           </div>
-        </div>
-        </div>
+          </div>
     </>
   );
 }
--- frontend/src/components/userText.tsx
+++ frontend/src/components/userText.tsx
@@ -2,7 +2,7 @@ function UserTextBubble({ message } : {message : string}) {
     return ( 
         <>
             <div className="pt-2 flex justify-center">
-                <div className="max-w-[720px] w-[80%] flex justify-end p-2 pr-8">
+                <div className="max-w-[720px] w-[80%] flex justify-end p-2 pr-5">
                     <div className="w-fit max-w-[78%] bg-[#2f2f2f] h-auto rounded-2xl pl-4 pr-4 pt-3 pb-3 text-white break-words whitespace-pre-wrap">
                         {message}
                     </div>
