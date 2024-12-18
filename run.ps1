6fcbc317719b6bccd9aaeee71be88227fc629c7c
moonlander101 <114925949+moonlander101@users.noreply.github.com>
2024-12-19 01:52:39 +0530

    Updated run script

--- run.ps1
+++ run.ps1
@@ -1,3 +1,7 @@
+# ===================================================
+# ==================== FUNCTIONS ====================
+# ===================================================
+
 function Get-BackendEnvVariables {
     param ()
     $envVariables = @{}
@@ -63,7 +67,7 @@ function Set-SavePathForChats($envFile) {
     Write-Host "Where would you like for your chats to be saved?" -ForegroundColor Cyan
     Write-Host "1. File System (default)" -ForegroundColor Cyan
     Write-Host "2. MongoDB" -ForegroundColor Cyan
-    Write-Host "Enter Value (Press Enter to skip): " -ForegroundColor Cyan -NoNewline
+    Write-Host "Enter Value (Press Enter for default): " -ForegroundColor Cyan -NoNewline
     $ChatSaveModeSelected = Read-Host
     if ($ChatSaveModeSelected -eq "2") {
         $newValue = "CHAT_SAVE_MODE=2"
@@ -75,8 +79,81 @@ function Set-SavePathForChats($envFile) {
     }
 }
 
-# MAIN
+function Set-SavePathForChatsInConfig($configPath, $config) {
+    Write-Host ""
+    Write-Host "Where would you like for your chats to be saved?" -ForegroundColor Cyan
+    Write-Host "1. File System (default)" -ForegroundColor Cyan
+    Write-Host "2. MongoDB" -ForegroundColor Cyan
+    Write-Host "Enter Value (Press Enter for default): " -ForegroundColor Cyan -NoNewline
+    $ChatSaveModeSelected = Read-Host
+    if ($ChatSaveModeSelected -eq "2") {
+        $config | Add-Member -NotePropertyName "CHAT_SAVE_MODE" -NotePropertyValue 2
+    } else {
+        $config | Add-Member -NotePropertyName "CHAT_SAVE_MODE" -NotePropertyValue 1
+    }
+    $res = $config | ConvertTo-Json -Depth 10
+    $res | Out-File -FilePath $configFile -Encoding utf8
+    return $config
+}
+
+function Set-ModelTypeInConfig($configFile, $config) {
+    Write-Host ""
+    Write-Host "Where Type of Text Completion Models are you using?" -ForegroundColor Cyan
+    Write-Host "1. OpenAI (default)" -ForegroundColor Cyan
+    Write-Host "2. HuggingFace" -ForegroundColor Cyan
+    Write-Host "Enter Value (Press Enter for default): " -ForegroundColor Cyan -NoNewline
+    $modelType = Read-Host
+    if ($modelType -eq "2") {
+        $config | Add-Member -NotePropertyName "MODEL_TYPE" -NotePropertyValue 2
+    } else {
+        $config | Add-Member -NotePropertyName "MODEL_TYPE" -NotePropertyValue 1
+    }
+    $res = $config | ConvertTo-Json -Depth 10
+    $res | Out-File -FilePath $configFile -Encoding utf8
+    return $config
+}
+
+function Set-HuggingFaceKey($envFile) {
+    Write-Host ""
+    $HFKey = ""
+    while ($HFKey.Length -eq 0) {
+        Write-Host "Enter your HuggingFace API key: " -ForegroundColor Cyan
+        Write-Host ">" -ForegroundColor Cyan -NoNewline
+        $HFKey = Read-Host -assecurestring
+
+        if ($HFKey.Length -eq 0) {
+            Write-Host "API key cannot be empty." -ForegroundColor Red
+        }
+    }
+    $HFKey = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
+        [Runtime.InteropServices.Marshal]::SecureStringToBSTR($HFKey)
+    )
+    $newValue = "HF_API_KEY=$HFKey"
+    $newValue | Add-Content -Path $envFile -Encoding UTF8
+}
+
+function Set-HuggingFaceURL($envFile) {
+    Write-Host ""
+    $HuggingFaceURL = ""
+    while ($HuggingFaceURL.Length -eq 0) {
+        Write-Host "Enter your HuggingFace Models URL: " -ForegroundColor Cyan
+        Write-Host ">" -ForegroundColor Cyan -NoNewline
+        $HuggingFaceURL = Read-Host
+
+        if ($HuggingFaceURL.Length -eq 0) {
+            Write-Host "URL cannot be empty." -ForegroundColor Red
+        }
+    }
+    $newValue = "BASE_URL=$HuggingFaceURL"
+    $newValue | Add-Content -Path $envFile -Encoding UTF8
+}
+
+
+# ===================================================
+# ==================== MAIN CODE ====================
+# ===================================================
 # Clear-Host
+
 
 Write-Host "  _      _      __  __   _____       _             __               " -ForegroundColor White
 Write-Host " | |    | |    |  \/  | |_   _|     | |           / _|              " -ForegroundColor White
@@ -87,33 +164,93 @@ Write-Host " |______|______|_|  |_| |_____|_| |_|\__\___|_|  |_| \__,_|\___\___|
 Write-Host "                                                                    "
 
 $envFile = ".\backend\.env"
-$exist = Test-Path $envFile -PathType Leaf
+$configFile = ".\backend\configs\config.json"
+$envExist = Test-Path $envFile -PathType Leaf
+$configExist = Test-Path $configFile  -PathType Leaf
 
+if (!$configExist) {
+    Write-Host "Backend config file does not exist" -ForegroundColor Red
+    Write-Host "Creating config file for backend" -ForegroundColor Yellow
+    New-Item -Path $configFile -ItemType "file" -Value "{}" | Out-Null
+} else {
+    Write-Host "Backend config file exists" -ForegroundColor Green
+}
 
-if ($exist) {
+if (!$envExist) {
+    Write-Host "Backend .env file does not exist" -ForegroundColor Red
+    Write-Host "Creating .env file for backend" -ForegroundColor Yellow
+    New-Item -Path $envFile -ItemType "file" | Out-Null
+} else {
     Write-Host "Backend .env file exists" -ForegroundColor Green
-    # Read all lines from the .env file
-    $envVariables = Get-BackendEnvVariables
-    
-    if ($envVariables["CHAT_SAVE_MODE"]) {
-        Write-Host "Chat save mode selected" -ForegroundColor Green
-    } else {
-        Set-SavePathForChats $envFile
-    }
+}
 
+$config = Get-Content $configFile | ConvertFrom-Json
+
+if ([bool]($config.PSobject.Properties.name -match "CHAT_SAVE_MODE")) {
+    Write-Host "CHAT_SAVE_MODE found in config" -ForegroundColor Green
+} else {
+    Write-Host "CHAT_SAVE_MODE not found in config" -ForegroundColor Red
+    $config = Set-SavePathForChatsInConfig $configFile $config
+}
+
+if ([bool]($config.PSobject.Properties.name -match "MODEL_TYPE")) {
+    Write-Host "MODEL_TYPE found in config" -ForegroundColor Green
+} else {
+    Write-Host "MODEL_TYPE not found in config" -ForegroundColor Red
+    $config = Set-ModelTypeInConfig $configFile $config
+}
+
+Write-Host ""
+Write-Host "Checking for environment variables..." -ForegroundColor Cyan
+Write-Host ""
+
+$envVariables = Get-BackendEnvVariables
+
+if ($config.MODEL_TYPE -eq 1) {
+    Write-Host "OpenAI Model selected" -ForegroundColor Green
     if ($envVariables["OPENAI_API_KEY"]) {
         Write-Host "OpenAI API key found" -ForegroundColor Green
     } else {
         Set-OpenAI $envFile
     }
+} else {
+    Write-Host "HuggingFace Model selected" -ForegroundColor Green
+    if ($envVariables["HF_API_KEY"]) {
+        Write-Host "HuggingFace API key found" -ForegroundColor Green
+    } else {
+        Set-HuggingFaceKey $envFile
+    }
 
+    if ($envVariables["BASE_URL"]) {
+        Write-Host "Base URL found" -ForegroundColor Green
+    } else {
+        Set-HuggingFaceURL $envFile
+    }
+}
+
+if ($config.CHAT_SAVE_MODE -eq 2) {
+    Write-Host "MongoDB selected" -ForegroundColor Green
+    
+    if ($envVariables["CONNECTION_URL"]) {
+        Write-Host "MongoDB Connection URL found" -ForegroundColor Green
+    } else {
+        Set-MongoVariables $envFile
+    }
 
 } else {
-    Write-Host "Backend .env file does not exist" -ForegroundColor Red
-    Write-Host "Creating .env file for backend" -ForegroundColor Yellow
-    Set-Content -Path $envFile -Value ""
-    Set-SavePathForChats $envFile
-    Set-OpenAI $envFile
+    Write-Host "File System selected" -ForegroundColor Green
+}
+
+if ($config.PSObject.Properties.Name -contains "model") {
+    Write-Host "Model found in config" -ForegroundColor Green
+} else {
+    Write-Host "Model not found in config" -ForegroundColor Red
+    Write-Host "Enter the model name: " -ForegroundColor Cyan
+    Write-Host ">" -ForegroundColor Cyan -NoNewline
+    $model = Read-Host
+    $config | Add-Member -NotePropertyName "model" -NotePropertyValue $model
+    $res = $config | ConvertTo-Json -Depth 10
+    $res | Out-File -FilePath $configFile -Encoding utf8
 }
 
 Write-Host "Setup Complete, Starting Chat" -ForegroundColor Green
